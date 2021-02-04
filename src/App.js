7acb54f298c53cc34fa5213a1624af65a88a2bc5
Juan Carlos Alvez <jcalvez@hotmail.com>
2021-02-04 13:13:26 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Connect4 from "./connect4";
 
 function App() {
+  const [game, setGame] = useState(0);
+  const [currentPlayer, setCurrentPlayer] = useState(1);
+  const [winner, setWinner] = useState(false);
+
+  useEffect(() => {
+    setGame(new Connect4());
+    console.log("Creando Instancia");
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {winner ? (
+        <div className="principal">Player {winner} Wins!</div>
+      ) : (
+        <div className="principal">Player ID: {currentPlayer}</div>
+      )}
+
+      <div className="principal">
+        {game &&
+          game.board.map((column, idx) => (
+            <div className="column" key={idx}>
+              <button
+                onClick={() => {
+                  game.addChipToColumn(idx);
+                  setCurrentPlayer(game.actualPlayerId);
+                  if (game.gotWinner) {
+                    setWinner(game.gotWinner);
+                    console.log("Ganador: ", winner);
+                  }
+                }}
+                key={idx + 1000}
+              >
+                Add Chip
+              </button>
+              {column
+                .slice(0)
+                .reverse()
+                .map((chip, idx2) => (
+                  <img
+                    src={"imgs/player" + chip + ".png"}
+                    className="chip"
+                    key={idx2}
+                    alt={"image " + idx2}
+                  />
+                ))}
+            </div>
+          ))}
+      </div>
+    </>
   );
 }
 
---
+++ src/connect4.js
@@ -0,0 +1,82 @@
+class Connect4 {
+    constructor() {
+        this.resetGame();
+    }
+
+    resetGame() {
+        this.gotWinner = false;
+        this.actualPlayerId = 1;
+        this.board = [
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0]
+        ]
+    }
+
+    switchPlayer() {
+        if (this.actualPlayerId === 1) { this.actualPlayerId = 2; } else { this.actualPlayerId = 1; }
+        console.log('Switched to Player ',this.actualPlayerId);
+    }
+
+    addChipToColumn(column) {
+        if (this.gotWinner) return false;
+        if (column < 0 || column > 6) return false;
+
+        for(let row=0; row < 6; row++) {
+            if (this.board[column][row] === 0) {
+                this.board[column][row] = this.actualPlayerId;
+                this.checkWinner();
+                this.switchPlayer();
+                return true;
+            }
+        }
+
+        return false;
+    }
+    
+    checkWinner() {
+			for (let col=0; col < (this.board[0].length - 3); col++) {
+				for (let row=0; row < this.board.length; row++) {
+					if (this.board[row][col] === this.actualPlayerId && this.board[row][col+1] === this.actualPlayerId && this.board[row][col+2] === this.actualPlayerId && this.board[row][col+3] === this.actualPlayerId) {
+                        this.gotWinner = this.actualPlayerId;
+                        return `${this.actualPlayerId} wins across!!!`;
+					}
+				}
+			}
+
+			for (let col=0; col < this.board[0].length; col++) {
+				for (let row=0; row < (this.board.length - 3); row++) {
+					if (this.board[row][col] === this.actualPlayerId && this.board[row+1][col] === this.actualPlayerId && this.board[row+2][col] === this.actualPlayerId && this.board[row+3][col] === this.actualPlayerId) {
+                        this.gotWinner = this.actualPlayerId;
+                        return `${this.actualPlayerId} wins vertically!!!`;
+					}
+				}
+			}  
+
+			for (let col=0; col < (this.board[0].length - 3); col++) {
+				for (let row=0; row < (this.board.length - 3); row++) { 
+					if (this.board[row][col] === this.actualPlayerId && this.board[row+1][col+1] === this.actualPlayerId && this.board[row+2][col+2] === this.actualPlayerId && this.board[row+3][col+3] === this.actualPlayerId) {
+                        this.gotWinner = this.actualPlayerId;
+                        return `${this.actualPlayerId} wins diagonally!!!`;
+					}
+				}
+			}
+
+			for (let col=0; col < (this.board[0].length - 3); col++) {
+				for (let row=3; row < this.board.length; row++) {
+					if (this.board[row][col] === this.actualPlayerId && this.board[row-1][col+1] === this.actualPlayerId && this.board[row-2][col+2] === this.actualPlayerId && this.board[row-3][col+3] === this.actualPlayerId) {
+                        this.gotWinner = this.actualPlayerId;
+                        return `${this.actualPlayerId} wins diagonally!!!`;
+					}
+				}
+			}  
+
+			return false;
+		}
+}
+
+export default Connect4;
